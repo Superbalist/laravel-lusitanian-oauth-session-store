b27bbd53c79366c74020053d8e316668f0afdfe4
Matthew Goslett <matthew@goslett.co.za>
2016-01-27 16:42:59 +0200

    initial code push

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # laravel-lusitanian-oauth-session-store
+
 A Laravel session storage interface for the lusitanian/oauth library
+
+[![Author](http://img.shields.io/badge/author-@superbalist-blue.svg?style=flat-square)](https://twitter.com/superbalist)
+[![Software License](https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square)](LICENSE)
+[![Packagist Version](https://img.shields.io/packagist/v/superbalist/laravel-lusitanian-oauth-session-store.svg?style=flat-square)](https://packagist.org/packages/superbalist/laravel-lusitanian-oauth-session-store)
+[![Total Downloads](https://img.shields.io/packagist/dt/superbalist/laravel-lusitanian-oauth-session-store.svg?style=flat-square)](https://packagist.org/packages/superbalist/laravel-lusitanian-oauth-session-store)
+
+
+## Installation
+
+```bash
+composer require superbalist/laravel-lusitanian-oauth-session-store
+```
+
+## Usage
+
+```php
+use OAuth\ServiceFactory;
+use Superbalist\LusitanianOAuth\LaravelTokenSessionStore;
+
+// this example demonstrates creating a github service
+
+$factory = new ServiceFactory();
+$storage = new LaravelTokenSessionStore();
+
+$credentials = [
+    '[[github key]]',
+    '[[github secret]]',
+    '[[url]]',
+];
+
+$gitHub = $factory->createService('GitHub', $credentials, $storage, array('user'));
+```
---
+++ src/Superbalist/LusitanianOAuth/LaravelTokenSessionStore.php
@@ -0,0 +1,170 @@
+<?php namespace Superbalist\LusitanianOAuth;
+
+use Illuminate\Session\Store;
+use OAuth\Common\Storage\Exception\AuthorizationStateNotFoundException;
+use OAuth\Common\Storage\Exception\TokenNotFoundException;
+use OAuth\Common\Storage\TokenStorageInterface;
+use OAuth\Common\Token\TokenInterface;
+
+class LaravelTokenSessionStore implements TokenStorageInterface
+{
+
+    protected $session;
+
+    /**
+     * @var string
+     */
+    protected $sessionVariableName;
+
+    /**
+     * @var string
+     */
+    protected $stateVariableName;
+
+    /**
+     * @param Store $session
+     * @param string $sessionVariableName
+     * @param string $stateVariableName
+     */
+    public function __construct(
+        Store $session,
+        $sessionVariableName = 'lusitanian_oauth_token',
+        $stateVariableName = 'lusitanian_oauth_token'
+    ) {
+        $this->session = $session;
+        $this->sessionVariableName = $sessionVariableName;
+        $this->stateVariableName = $stateVariableName;
+
+        if (! $this->session->has($this->sessionVariableName)) {
+            $this->session->put($this->sessionVariableName, array());
+        }
+
+        if (! $this->session->has($this->stateVariableName)) {
+            $this->session->put($this->stateVariableName, array());
+        }
+    }
+
+    /**
+     * @param string $service
+     * @return TokenInterface
+     * @throws \OAuth\Common\Storage\Exception\TokenNotFoundException
+     */
+    public function retrieveAccessToken($service)
+    {
+        if ($this->hasAccessToken($service)) {
+            return $this->session->get($this->sessionVariableName . '.' . $service);
+        }
+
+        throw new TokenNotFoundException('Token not found in session, are you sure you stored it?');
+    }
+
+    /**
+     * @param string $service
+     * @param TokenInterface $token
+     * @return TokenStorageInterface
+     */
+    public function storeAccessToken($service, TokenInterface $token)
+    {
+        $accessTokens = $this->session->get($this->sessionVariableName, array());
+        $accessTokens[$service] = $token;
+
+        $this->session->put($this->sessionVariableName, $accessTokens);
+
+        // allow chaining
+        return $this;
+    }
+
+    /**
+     * @param string $service
+     * @return bool
+     */
+    public function hasAccessToken($service)
+    {
+        return $this->session->has($this->sessionVariableName . '.' . $service);
+    }
+
+    /**
+     * @param string $service
+     * @return TokenStorageInterface
+     */
+    public function clearToken($service)
+    {
+        $this->session->forget($this->sessionVariableName . '.' . $service);
+
+        // allow chaining
+        return $this;
+    }
+
+    /**
+     * @return TokenStorageInterface
+     */
+    public function clearAllTokens()
+    {
+        $this->session->forget($this->sessionVariableName);
+
+        // allow chaining
+        return $this;
+    }
+
+    /**
+     * @param string $service
+     * @param string $state
+     * @return TokenStorageInterface
+     */
+    public function storeAuthorizationState($service, $state)
+    {
+        $states = $this->session->get($this->stateVariableName, array());
+        $states[$service] = $state;
+
+        $this->session->put($this->stateVariableName, $states);
+
+        // allow chaining
+        return $this;
+    }
+
+    /**
+     * @param string $service
+     * @return bool
+     */
+    public function hasAuthorizationState($service)
+    {
+        return $this->session->has($this->stateVariableName . '.' . $service);
+    }
+
+    /**
+     * @param string $service
+     * @return string
+     * @throws \OAuth\Common\Storage\Exception\AuthorizationStateNotFoundException
+     */
+    public function retrieveAuthorizationState($service)
+    {
+        if ($this->hasAuthorizationState($service)) {
+            return $this->session->get($this->stateVariableName . '.' . $service);
+        }
+
+        throw new AuthorizationStateNotFoundException('State not found in session, are you sure you stored it?');
+    }
+
+    /**
+     * @param string $service
+     * @return TokenStorageInterface
+     */
+    public function clearAuthorizationState($service)
+    {
+        $this->session->forget($this->stateVariableName . '.' . $service);
+
+        // allow chaining
+        return $this;
+    }
+
+    /**
+     * @return TokenStorageInterface
+     */
+    public function clearAllAuthorizationStates()
+    {
+        $this->session->forget($this->stateVariableName);
+
+        // allow chaining
+        return $this;
+    }
+}
